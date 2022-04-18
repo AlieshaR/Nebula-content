41f2dfcc6babfb6653d9608f0d75d47074eaae05
AlieshaR <aliesharedmond3@gmail.com>
2022-04-18 17:28:12 -0400

    variables-practice

--- 1-variables/practice/variable-practice.js
+++ 1-variables/practice/variable-practice.js
@@ -4,27 +4,27 @@
 */  
   
   // 1. Declare a new variable and set it equal to the string of your name
-  const name= "Aliesha"
-  console.log
+  const firstName= "Aliesha"
+  console.log(firstName)
   // 2. Declare a new variable and set it equal to a number 
-   const number= 30
-   console.log
+   const anyNum= 30
+   console.log(anyNum)
   // 3. Set a variable to a boolean value 
-   const isopen = true
-   console.log
+   const isOpen = true
+   console.log(isOpen)
   // 4. Create a variable called carName, assign the value Volvo to it.
    const carName= "Volvo"
-   console.log
+   console.log(carName)
   // 5. Create a variable called num, assign the value 50 to it.
    const num= 50
-  console.log
+  console.log(num)
   // 6. Display the sum of 5 + 10, using two variables: num1 and num2.
 
-  const num1="5",
+  const num1="5"
   const num2="10"
   console.log(num1+num2)
   
   // 7. Create a variable called num3, assign the addition of num1 and num2 to it.
    const num3= num1 + num2
-   console.log
+   console.log(num3)
 
