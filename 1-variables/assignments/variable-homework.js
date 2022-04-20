9f042d18321b5e4dd97eabdb0fa0c9dd66011d15
AlieshaR <aliesharedmond3@gmail.com>
2022-04-20 18:06:24 -0400

    variableshw

--- 1-variables/assignments/variable-homework.js
+++ 1-variables/assignments/variable-homework.js
@@ -3,5 +3,21 @@
 // For each exercise you'll want to log the result to the console to check your work
 
 //1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+const animalAge = "5"
+const animalHeight = "4 ft"
+const animalName = "rabbit"
+const animalType = "rodent"
+const animalColor = "grey"
+const animalShape = "oval"
+
+console.log(animalAge, animalHeight, animalName, animalType, animalColor, animalShape)
 
 //2. Create a set of variables that define a vehicle. Must be at least 6 variables.cd
+const vehicleBrand = "chevorlet"
+const vehicleType = "suv"
+const vehicleColor = "white"
+const vehicleCylinders = "6"
+const vehicleInterior = "leather"
+const vehicleWheels = "Alloy"
+
+console.log(vehicleBrand, vehicleType, vehicleColor, vehicleCylinders, vehicleInterior, vehicleWheels)
---
+++ 2-numbers/practice/numbers-practice.js
@@ -0,0 +1,20 @@
+// Add together two numbers.
+2+2;4
+// - Subtract two numbers.
+4-2;2
+// - Multiply two numbers.
+2*2;4
+// - Divide two numbers.
+4/2;2
+// - Find the remainder of 223 divided by 3.
+33+infinity
+// - Store the value in a variable produced by dividing 300 by 100. Declare that value with the `let` keyword.
+let answer = 3
+// - Increment that variable by 1 using the increment operator.
+3(++)
+// - console.log that variable, or type it directly in the console to view it's value.
+console.log(3++)
+// - Use a multiply assignment operator to change the value of the variable to the product of multiplying it by 5.
+3*5;15
+// - console.log that variable, or type it directly in the console to view it's value.
+console.log(3*5) = 15
